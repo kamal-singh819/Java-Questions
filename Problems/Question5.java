88779920370614180c02ea2b286e9ac91fa3b877
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2022-12-28 02:56:41 +0530

    Create Question5.java

---
+++ Problems/Question5.java
@@ -0,0 +1,8 @@
+//A java program to the values of boolena variables
+public class Question5 {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+        System.out.print(x + " " + y);
+    }
+}
