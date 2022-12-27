8e694cb80cf3920d9f8abdb842206d78d84150f2
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2022-12-28 02:59:03 +0530

    Create Question10.java

---
+++ Problems/Question10.java
@@ -0,0 +1,13 @@
+//A java program to show the use of ?: operator
+
+public class Question10 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c;
+
+        c = a > b ? a : b;
+
+        System.out.println("Maximum of a and b is " + c);
+    }
+}
