0007f1766126c46495138c6925d177e305df63c4
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2022-12-28 02:57:55 +0530

    Create Question8.java

---
+++ Problems/Question8.java
@@ -0,0 +1,12 @@
+//A java program to create an integer array, initilize it and print values
+
+public class Question8 {
+    public static void main(String[] args) {
+        int array[] = new int[] { 1, 3, 4, 5, 6, 7, 8 };
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            System.out.print(" ");
+        }
+    }
+}
