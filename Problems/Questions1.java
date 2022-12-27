b3e718a4ab4ee2f1aded55f91d0e4290516d2aef
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2022-12-28 02:54:27 +0530

    Create Questions1.java

---
+++ Problems/Questions1.java
@@ -0,0 +1,8 @@
+//Java Program to show your Name and ROll Number
+public class Question1 {
+    public static void main(String[] args) {
+        int rollNo = 20201001;
+        String name = "Kamal Singh";
+        System.out.println("Name is " + name + " and Roll Number is " + rollNo);
+    }
+}
